dafd2bbd9dcda58fba494f6e9f70978f8c39e33c
maitreyee nath <maitreyeenath30798@gmail.com>
2022-06-07 19:56:20 +0530

    fix: Updated import in router

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import { Home, Explore, Profile, UserPost, BookmarkedPost } from "../pages";
+import {
+  Home,
+  Explore,
+  Profile,
+  UserPost,
+  BookmarkedPost,
+  SinglePost,
+} from "../pages";
 
 const Router = () => {
   return (
