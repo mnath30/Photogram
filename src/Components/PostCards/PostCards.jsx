c30eb157835ffcdabb48e9576010472d0f8c57e0
maitreyee nath <maitreyeenath30798@gmail.com>
2022-06-13 17:45:13 +0530

    fix: fixed class names

--- src/Components/PostCards/PostCards.jsx
+++ src/Components/PostCards/PostCards.jsx
@@ -3,21 +3,21 @@ import "./post-cards.css";
 const PostCards = ({ item }) => {
   const { username, profile, info, image, description } = item;
   return (
-    <div className="post__container flex-col">
-      <div className="flex post__container-header padding-sm">
+    <div className="postcard__container flex-col">
+      <div className="flex postcard__container-header padding-sm">
         <img src={profile} alt={username} className="avatar" />
         <h4 className="padding-sm">{username}</h4>
       </div>
-      <div className="post__container-image">
+      <div className="postcard__container-image">
         <img src={image} alt={info} className="responsive-img" />
       </div>
-      <div className="post__container-content flex-col">
+      <div className="postcard__container-content flex-col">
         <div className="flex padding-sm">
           <i className="fa-regular fa-heart fa-lg padding-sm"></i>
           <i className="fa-regular fa-bookmark fa-lg padding-sm"></i>
         </div>
 
-        <p className="post__container-description padding-sm">
+        <p className="postcard__container-description padding-sm">
           <strong>{username} </strong>
           {description}
         </p>
@@ -28,7 +28,7 @@ const PostCards = ({ item }) => {
             placeholder="Add a comment..."
             className="padding-sm"
           />
-          <button className="post__container-btn">Post</button>
+          <button className="postcard__container-btn">Post</button>
         </div>
       </div>
     </div>
--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -4,9 +4,9 @@ const Posts = ({ post }) => {
   const { info, image } = post;
   return (
     <>
-      <div className="singlepost__container">
-        <img src={image} alt={info} className="singlepost__img" />
-        <div className="singlepost__overlay flex">
+      <div className="post__container">
+        <img src={image} alt={info} className="post__img" />
+        <div className="post__overlay flex">
           <i className="fa-solid fa-heart fa-lg padding-sm">
             <span className=" padding-sm">23</span>
           </i>
