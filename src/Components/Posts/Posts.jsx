18e3f472a2954ad100ad3bdc291768c9bb02a9ec
maitreyee nath <maitreyeenath30798@gmail.com>
2022-07-14 23:26:54 +0530

    feat: Added link to explore posts
    Added single post page link to explore posts

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,10 +1,13 @@
+import { useNavigate } from "react-router-dom";
 import "./posts.css";
 
 const Posts = ({ post }) => {
-  const { info, image, likes, comments } = post;
+  const navigate = useNavigate();
+  const { _id, info, image, likes, comments } = post;
+
   return (
     <>
-      <div className="post__container">
+      <div className="post__container" onClick={() => navigate(`/post/${_id}`)}>
         <img src={image} alt={info} className="post__img" />
         <div className="post__overlay flex">
           <i className="fa-solid fa-heart fa-lg padding-sm">
